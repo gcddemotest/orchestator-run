a12b8e76b16e15b526dc248e7b1237e2e79f4e7f
marco.siviero@jobrapido.com <marco.siviero@jobrapido.com>
2019-09-10 14:56:10 +0200

    use @env

--- src/provider/logger-provider.ts
+++ src/provider/logger-provider.ts
@@ -1,14 +1,22 @@
-import { provider, Scope } from "@msiviero/knit";
+import { provider, Scope, env } from "@msiviero/knit";
 import * as pino from "pino";
 import { Logger } from "pino";
 
 @provider<Logger>("app:log", Scope.Singleton)
 export class LoggerProvider {
 
+  constructor(
+    @env("LOG_LEVEL", "info") private readonly logLevel: string,
+    @env("NODE_ENV", "development") private readonly nodeEnv: string,
+  ) { }
+
   public provide = () => {
+
+    console.log("blabal", this.logLevel);
+
     return pino({
-      prettyPrint: (process.env.NODE_ENV || "") === "production" ? false : { colorize: true },
-      level: process.env.LOG_LEVEL || "info",
+      prettyPrint: this.nodeEnv ? false : { colorize: true },
+      level: this.logLevel,
     });
   }
 }
