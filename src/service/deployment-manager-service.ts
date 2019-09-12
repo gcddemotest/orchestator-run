1e129cd9a88215477d27e032db7aa73cd1a673ee
marco.siviero@jobrapido.com <marco.siviero@jobrapido.com>
2019-09-12 15:27:57 +0200

    Fixed last apis

--- src/service/deployment-manager-service.ts
+++ src/service/deployment-manager-service.ts
@@ -4,8 +4,10 @@ import { Logger } from "pino";
 import { GoogleApiClient } from "../provider/googleapi-provider";
 
 export interface Operation {
-  endTime?: string;
-  insertTime?: string;
+  startTime: string;
+  insertTime: string;
+  status: string; // "RUNNING" | "DONE";
+  operationType: string; // insert" | "delete";
 }
 
 @injectable()
@@ -15,42 +17,6 @@ export class DeploymentManagerService {
     @inject("google:client") private readonly googleApiClient: Promise<GoogleApiClient>,
     @inject("app:log") private log: Logger,
   ) { }
-
-  /*
-  {
-  operationType: "insert",
-  status: "RUNNING", | "DONE"
-  insertTime: "2019-09-12T05:57:56.330-07:00",
-  startTime: "2019-09-12T05:57:56.336-07:00",
-  kind: "deploymentmanager#operation"
-  }
-  */
-  public async get(): Promise<Readonly<Operation>> {
-
-    try {
-      const google = await this.googleApiClient;
-
-      const response = await google
-        .client
-        .deploymentmanager("v2beta")
-        .deployments
-        .get({
-          project: google.projectId,
-          deployment: "managed-by-orchestator",
-        });
-
-      const operation = response.data.operation;
-
-      if (operation) {
-        return operation;
-      } else {
-        throw new HttpError(404, "No operation pending");
-      }
-    } catch (error) {
-      this.log.error("Error while getting deployment state", error);
-      throw error;
-    }
-  }
 
   public async deploy() {
 
@@ -97,6 +63,40 @@ export class DeploymentManagerService {
         });
     } catch (error) {
       this.log.error("Error while destroying deployment", error);
+      throw error;
+    }
+  }
+
+
+  public async get(): Promise<Readonly<Partial<Operation>>> {
+
+    try {
+      const google = await this.googleApiClient;
+      const response = await google
+        .client
+        .deploymentmanager("v2beta")
+        .deployments
+        .get({
+          project: google.projectId,
+          deployment: "managed-by-orchestator",
+        });
+
+      const operation = response.data.operation;
+
+      if (operation) {
+        const {
+          startTime,
+          insertTime,
+          status,
+          operationType,
+        } = operation;
+
+        return { startTime, insertTime, status, operationType };
+      } else {
+        throw new HttpError(404, "No operation pending");
+      }
+    } catch (error) {
+      this.log.error("Error while getting deployment state", error);
       throw error;
     }
   }
