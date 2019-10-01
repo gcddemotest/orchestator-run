10d0771bf3a9bd7ed05bf5f3ae73c8d90f987bc2
Gianluca Moretti <gianluca.moretti@jobrapido.com>
2019-10-01 16:32:58 +0200

    Removed no more used file

--- deployment-scripts/scripts/frontend-vm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# export metadata configs to env 
-touch /opt/metadata.yaml
-
-echo "blackbox-address: $(curl -H Metadata-Flavor:Google http://metadata/computeMetadata/v1/instance/attributes/blackbox-address)" >> /opt/metadata.yaml
-echo "docker-app-address: $(curl -H Metadata-Flavor:Google http://metadata/computeMetadata/v1/instance/attributes/docker-app-address)" >> /opt/metadata.yaml
